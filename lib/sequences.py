5e4808c7581ffb79f774fe89d36c2b4fa71b0c17
Caleb Kiptoo <vincentcalebkip@gmail.com>
2025-02-13 12:33:00 +0300

    fibonacci series

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    sequence = [0, 1]
+    for _ in range(length-2):
+        sequence.append(sequence[-1] + sequence[-2])
+    print(sequence[:length])
     pass
+
+print(print_fibonacci(10))
